07dab1ebd4045f0c9c5beffa9b347dbdb29858b5
Sergio Marcelino <sergio@filho.org>
2026-02-26 09:43:21 -0300

    fix: build issue

--- packages/node/iii/src/index.ts
+++ packages/node/iii/src/index.ts
@@ -4,7 +4,7 @@ export { getContext, withContext, type Context } from './context'
 export { Logger } from './logger'
 export type { ISdk } from './types'
 
-export type { ApiRequest, HttpRequest, HttpResponse, ApiResponse, ApiStreamResponse, Channel } from './types'
+export type { ApiRequest, HttpRequest, HttpResponse, ApiResponse, Channel } from './types'
 export type { StreamChannelRef } from './iii-types'
 export { ChannelWriter, ChannelReader } from './channels'
 
